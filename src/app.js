672528ca23e1bac88731eb8f2c2f4147e2afc035
Thanasis759 <th.stefanidis@gmail.com>
2023-06-28 22:26:19 +0200

    Heroku fix

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const hbs = require('hbs')
 const app = express()
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 //Define paths for express config
 const publicFolder = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
